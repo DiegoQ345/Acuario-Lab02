463e5398fe757b39685232bdefa58d999f101eaa
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 14:55:19 -0500

    Prueba del getter

--- Acuario-POO/src/acuario/main.kt
+++ Acuario-POO/src/acuario/main.kt
@@ -2,9 +2,11 @@ package acuario
 import Acuario.Acuario
 
 fun construirAcuario() {
-    val acuario6 = Acuario(numeroDePeces = 29)
-    acuario6.imprimirTamano()
-    println("Volumen: ${acuario6.ancho * acuario6.largo * acuario6.alto / 1000} l")
+
+
+    val acuario1 = Acuario(alto = 31)
+    acuario1.imprimirTamano()
+
 }
 
 fun main() {
