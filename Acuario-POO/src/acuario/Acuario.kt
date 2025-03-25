d53d0520c05a7b208f5bfcaaeb3127871ca911d9
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 14:14:30 -0500

    Creacion del metodo Imprimir tamaño

--- Acuario-POO/src/acuario/Acuario.kt
+++ Acuario-POO/src/acuario/Acuario.kt
@@ -5,3 +5,10 @@ class Acuario {
     var alto: Int = 40
     var largo: Int = 100
 }
+
+
+fun imprimirTamano() {
+    println("Ancho: $ancho cm " +
+            "Largo: $largo cm " +
+            "Alto: $alto cm ")
+}
