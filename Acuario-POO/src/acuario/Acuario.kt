49955a36aa444b508f334ea51ed6a0a2f4b74076
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 14:43:05 -0500

    Creación del constructor secundario

--- Acuario-POO/src/acuario/Acuario.kt
+++ Acuario-POO/src/acuario/Acuario.kt
@@ -14,6 +14,14 @@ class Acuario(largo: Int = 100, ancho: Int = 20, alto: Int = 40) {
         )
     }
 
+    constructor(numeroDePeces: Int) : this() {
+        // 2,000 cm^3 por pez + espacio extra para que no se derrame
+        val tanque = numeroDePeces * 2000 * 1.1
+        alto = (tanque / (largo * ancho)).toInt()
+    }
+
+
+
     init {
         println("inicializando acuario")
     }
@@ -23,7 +31,10 @@ class Acuario(largo: Int = 100, ancho: Int = 20, alto: Int = 40) {
         println("Volumen: ${ancho * largo * alto / 1000} l")
 
     }
+
 }
+
+
 
 
 
