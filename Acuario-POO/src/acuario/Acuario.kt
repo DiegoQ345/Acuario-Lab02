34ab7d4fa28a2e05aa5369d484f7dc24ba0c6322
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 14:30:47 -0500

    Constructor de la clase acuario

--- Acuario-POO/src/acuario/Acuario.kt
+++ Acuario-POO/src/acuario/Acuario.kt
@@ -1,15 +1,20 @@
 package Acuario
 
-class Acuario {
-    var ancho: Int = 20
-    var alto: Int = 40
-    var largo: Int = 100
+class Acuario(largo: Int = 100, ancho: Int = 20, alto: Int = 40) {
+        var largo: Int = largo
+        var ancho: Int = ancho
+        var alto: Int = alto
 
-    fun imprimirTamano() {
-        println("Ancho: $ancho cm " +
-                "Largo: $largo cm " +
-                "Alto: $alto cm ")
+        fun imprimirTamano() {
+            println(
+                "Ancho: $ancho cm " +
+                        "Largo: $largo cm " +
+                        "Alto: $alto cm "
+            )
+        }
     }
-}
+
+
+
 
 
