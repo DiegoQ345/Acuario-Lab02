c014d38726746fbf07c4f02fec5a013360bc7dff
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 14:34:39 -0500

    Implementación de inicializadores

--- Acuario-POO/src/acuario/Acuario.kt
+++ Acuario-POO/src/acuario/Acuario.kt
@@ -1,20 +1,29 @@
 package Acuario
 
 class Acuario(largo: Int = 100, ancho: Int = 20, alto: Int = 40) {
-        var largo: Int = largo
-        var ancho: Int = ancho
-        var alto: Int = alto
+    var largo: Int = largo
+    var ancho: Int = ancho
+    var alto: Int = alto
 
-        fun imprimirTamano() {
-            println(
-                "Ancho: $ancho cm " +
-                        "Largo: $largo cm " +
-                        "Alto: $alto cm "
-            )
-        }
+
+    fun imprimirTamano() {
+        println(
+            "Ancho: $ancho cm " +
+                    "Largo: $largo cm " +
+                    "Alto: $alto cm "
+        )
     }
 
+    init {
+        println("inicializando acuario")
+    }
 
+    init {
+        // 1 litro = 1000 cm^3
+        println("Volumen: ${ancho * largo * alto / 1000} l")
+
+    }
+}
 
 
 
