7a1522ab16dc83b481e13e25ea831ca2b7820ab9
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 14:54:55 -0500

    Implementacion del getter

--- Acuario-POO/src/acuario/Acuario.kt
+++ Acuario-POO/src/acuario/Acuario.kt
@@ -7,17 +7,24 @@ class Acuario(largo: Int = 100, ancho: Int = 20, alto: Int = 40) {
 
 
     fun imprimirTamano() {
-        println(
-            "Ancho: $ancho cm " +
-                    "Largo: $largo cm " +
-                    "Alto: $alto cm "
+        println("Ancho: $ancho cm " +
+                "Largo: $largo cm " +
+                "Alto: $alto cm "
         )
+        // 1 l = 1000 cm^3
+        println("Volumen: $volumen l")
     }
+
+
+    val volumen: Int
+        get() = ancho * alto * largo / 1000  // 1000 cm^3 = 1 l
+
 
     constructor(numeroDePeces: Int) : this() {
         // 2,000 cm^3 por pez + espacio extra para que no se derrame
         val tanque = numeroDePeces * 2000 * 1.1
         alto = (tanque / (largo * ancho)).toInt()
+
     }
 
 
@@ -26,11 +33,6 @@ class Acuario(largo: Int = 100, ancho: Int = 20, alto: Int = 40) {
         println("inicializando acuario")
     }
 
-    init {
-        // 1 litro = 1000 cm^3
-        println("Volumen: ${ancho * largo * alto / 1000} l")
-
-    }
 
 }
 
