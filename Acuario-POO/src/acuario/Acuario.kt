7e2e0a47cac6d3e958cc03e5f342388837658c88
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 15:09:11 -0500

    Implementación del Open en la clase Acuario

--- Acuario-POO/src/acuario/Acuario.kt
+++ Acuario-POO/src/acuario/Acuario.kt
@@ -1,26 +1,29 @@
 package Acuario
 
-class Acuario(largo: Int = 100, ancho: Int = 20, alto: Int = 40) {
-    var largo: Int = largo
-    var ancho: Int = ancho
-    var alto: Int = alto
-
-
-    fun imprimirTamano() {
-        println("Ancho: $ancho cm " +
-                "Largo: $largo cm " +
-                "Alto: $alto cm "
-        )
-        // 1 l = 1000 cm^3
-        println("Volumen: $volumen l")
-    }
-
-
-    var volumen: Int
-        get() = ancho * alto * largo / 1000  // 1000 cm^3 = 1 l
+open class Acuario (
+    open var largo: Int = 100,
+    open var ancho: Int = 20,
+    open var alto: Int = 40
+) {
+    open var volumen: Int
+        get() = ancho * alto * largo / 1000
         set(valor) {
             alto = (valor * 1000) / (ancho * largo)
         }
+    open val forma = "rectángulo"
+
+    open var agua: Double = 0.0
+        get() = volumen * 0.9
+
+
+    fun imprimirTamano() {
+        println(forma)
+        println("Ancho: $ancho cm " +
+                "Largo: $largo cm " +
+                "Alto: $alto cm ")
+        println("Volumen: $volumen l Agua: $agua l (${agua/volumen*100.0}% lleno)")
+    }
+
 
     constructor(numeroDePeces: Int) : this() {
         // 2,000 cm^3 por pez + espacio extra para que no se derrame
@@ -28,8 +31,6 @@ class Acuario(largo: Int = 100, ancho: Int = 20, alto: Int = 40) {
         alto = (tanque / (largo * ancho)).toInt()
 
     }
-
-
 
     init {
         println("inicializando acuario")
