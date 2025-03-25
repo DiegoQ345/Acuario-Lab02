b117cc7a82d6576fb27c01c3deac377ac483b07d
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 15:09:39 -0500

    Prueba del codigo

--- Acuario-POO/src/acuario/main.kt
+++ Acuario-POO/src/acuario/main.kt
@@ -7,6 +7,9 @@ fun construirAcuario() {
     acuario7.imprimirTamano()
     acuario7.volumen = 70
     acuario7.imprimirTamano()
+    val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario8.imprimirTamano()
+
 
 }
 
