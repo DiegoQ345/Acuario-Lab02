90e11d94a713bb5dd94c37082615d5c329269b30
Diego <Diego.quispe.qui@tecsup.edu.pe>
2025-03-25 15:16:53 -0500

    Acuario: Herencia Terminado

--- Acuario-POO/src/acuario/main.kt
+++ Acuario-POO/src/acuario/main.kt
@@ -4,7 +4,12 @@ import Acuario.TanqueTorre
 
 fun construirAcuario() {
 
-    
+    val acuario7 = Acuario(numeroDePeces = 29)
+    acuario7.imprimirTamano()
+    acuario7.volumen = 70
+    acuario7.imprimirTamano()
+    val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario8.imprimirTamano()
     val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
     miAcuario.imprimirTamano()
     val miTorre = TanqueTorre(diametro = 25, alto = 40)
